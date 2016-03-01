c4f0f12065e02b568fe93e222409a50621be2c91
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-03-01 18:14:08 -0500

    Passing spec

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,20 +1,26 @@
 function ContactController() {
-    this.contacts = [{
-        name: 'Bob',
-        phone: '0123458690'
-    },{
-        name: 'Tim',
-        phone: '3934203242'
-    },{
-        name: 'Ross',
-        phone: '0684059433'
-    }];
+  this.contacts = [{
+    name: 'Bob',
+    phone: '0123458690'
+  }, {
+    name: 'Tim',
+    phone: '3934203242'
+  }, {
+    name: 'Ross',
+    phone: '0684059433'
+  }];
 
-    this.removeContact = function (index) {
-        this.contacts.splice(index, 1);
-    }
+  this.removeContact = function(index) {
+    this.contacts.splice(index, 1);
+  }
+  this.addContact = function(newName, phoneNumber) {
+    this.contacts.push({
+      name: newName,
+      phone: phoneNumber
+    })
+  }
 }
 
 angular
-    .module('app')
-    .controller('ContactController', ContactController);
+  .module('app')
+  .controller('ContactController', ContactController);
